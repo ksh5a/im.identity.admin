55515821043e6c83c03b3b070f9dd0e2cc9a38b4
Razvan Predescu <rzvdaniel@yahoo.com>
2014-08-23 18:15:02 +0300

    Initial version

---
+++ IM.Identity.DB/Scripts/Principal.sql
@@ -0,0 +1,13 @@
+﻿IF NOT EXISTS 
+    (SELECT name  
+     FROM master.sys.server_principals
+     WHERE name = 'imidentity')
+BEGIN
+    CREATE LOGIN metrics WITH PASSWORD = 'imidentitypassword'
+END
+
+GO
+-- Creates a database user for the login created above.
+CREATE USER imidentity FOR LOGIN imidentity;
+exec sp_addrolemember 'db_owner', 'imidentity'
+GO
---
+++ IM.Identity.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IM.Identity.Web.MvcApplication" Language="C#" %>
